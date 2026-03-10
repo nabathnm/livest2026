5436a40db540bf1c275c468e2faeefd1b46bc1eb
Nabath Nuur <muhammadnabathnuur@gmail.com>
2026-03-10 16:52:48 +0700

    Revert "update terbaru login/register/profile"

--- lib/features/buyer/buyer_main_page.dart
+++ lib/features/buyer/buyer_main_page.dart
@@ -6,7 +6,7 @@ import 'package:livest/features/buyer/home/pages/home_page.dart';
 import 'package:livest/features/buyer/profile/pages/profile_page.dart';
 import 'package:livest/features/buyer/cart/pages/chart_page.dart';
 
-class BuyerMainPage extends StatelessWidget {
+class BuyerMainPage extends StatefulWidget {
   const BuyerMainPage({super.key});
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
-
 import 'package:livest/livest.dart';
-import 'package:livest/features/auth/providers/auth_provider.dart';
-import 'package:livest/features/auth/providers/profile_provider.dart';
-import 'package:livest/features/breader/home/providers/education_provider.dart';
-import 'package:livest/features/breader/ai/providers/chat_provider.dart';
-import 'package:livest/core/providers/navigation_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
   await dotenv.load(fileName: ".env");
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => ProfileProvider()),
-        ChangeNotifierProvider(create: (_) => EducationProvider()),
-        ChangeNotifierProvider(create: (_) => ChatProvider()),
-        ChangeNotifierProvider(create: (_) => NavigationProvider()),
-      ],
-      child: const Livest(),
-    ),
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+
+  runApp(const Livest());
 }
